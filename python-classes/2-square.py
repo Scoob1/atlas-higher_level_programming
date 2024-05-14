64d4b3783d3eb1775df69ee5b4ae2bfe1f9c3c3f
Scoob1 <tamara_walling@yahoo.com>
2024-05-14 03:30:45 +0000

    task 1-2 square

---
+++ python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module - 2-square
+Defines a class Square with private atribute size
+"""
+
+class Square:
+    """
+    Defines a square
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes the Square instance
+        """
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")

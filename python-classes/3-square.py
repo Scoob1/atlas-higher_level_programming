e945785cb7d357b551c7498516a3a396a1f15920
Scoob1 <ttamara_walling@yahoo.com>
2024-05-14 17:12:04 +0000

    added self size line

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -20,6 +20,8 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
 
+        self.__size = size
+
         def area(self):
             """
             Computes the area of the square

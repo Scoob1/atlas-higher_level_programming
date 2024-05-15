db7bc624b8f98d4166bb67592608e7d1c84f7882
Scoob1 <tamara_walling@yahoo.com>
2024-05-15 20:52:01 +0000

    0123 locally

---
+++ python-import_modules/3-infinite_add.pyx
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+
+    total = 0
+    
+for arg in sys.argv[1:]:
+    try:
+        total += int(arg)
+    except ValueError:
+        pass
+
+print("{}".format(total))

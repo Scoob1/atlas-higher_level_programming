8985ea1b24f6073760fb2270f5a259e79d328407
Scoob1 <tamara_walling@yahoo.com>
2024-05-15 22:00:12 +0000

    data structures

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
+    else:
+        for row in matrix:
+            for element in row:
+                if element != row[-1]:
+                    print("{:d}".format(element), end=" ")
+                else:
+                    print("{:d}".format(element))

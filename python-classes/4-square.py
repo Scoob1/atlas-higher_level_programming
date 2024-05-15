093798c7740b16500c445dfca2606383eb91e55e
Scoob1 <ttamara_walling@yahoo.com>
2024-05-15 00:35:51 +0000

    added multiply 2

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-""" write a class square tha defines a square:"""
+""" Defining a class Square """
 
 
 class Square:
-    """
-    Class that defines a square with size validation, and property methods to access and update size.
-    """
-
+    """Represent a Square """
     def __init__(self, size=0):
-        self.size = size
+        """ init method initializing a size attribute
+                Args:
+                size: (obj: int) Size is calulating the area of a square"""
+        self.__size = size
 
-        @property
-        def size(self):
-            """Getter for size."""
-            return self.__size
+    @property
+    def size(self):
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            """Setter for size with validation."""
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size mustt be >+ 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
-            def area(self):
-                """Returns the area of the square."""
-                return self.__size * self.__size
+    def area(self):

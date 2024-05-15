74f06dda1a5806a91f179d01c210c1702f6b4d88
Scoob1 <tamara_walling@yahoo.com>
2024-05-15 22:37:43 +0000

    args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
+
+
+def print_args(argv):
+    if len(argv) - 1 == 0:
+        print("0 arguments.")
+    elif len(argv) - 1 == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
+
 
 if __name__ == "__main__":
-
-    argc = len(sys.argv) - 1
-
-if argc < 1:
-    print("0 arguments.")
-elif argc == 1:
-    print("1 argument.")
-    print("1: {}".format(sys.argv[1]))
-else:
-    print("{} arguments:".format(argc))
-    for i in range(1, argc + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+    print_args(argv)

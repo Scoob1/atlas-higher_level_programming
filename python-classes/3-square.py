96494919d73ce60126631aa5801c29feb3e75cb7
Scoob1 <ttamara_walling@yahoo.com>
2024-05-15 00:34:23 +0000

    literally did the same thing maybe subspace

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-""" Defining a class square """
+""" Defining a class Square """
 
 
 class Square:
-    """
-    Defines a square with size validation
-    """
-
+    """Represent a Square """
     def __init__(self, size=0):
+        """ init method initializing a size attribute
+                Args:
+                size: (obj: int) Size is calulating the area of a square"""
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
+
         self.__size = size
 
-        def area(self):
-            """
-            Returns the area of the square
-            """
-            return self.__size * self.__size
+    def area(self):
+        return self.__size**2

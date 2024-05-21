3e9caa3e3522911a1b9f37348a194fa09e59e29b
Scoob1 <tamara_walling@yahoo.com>
2024-05-21 21:23:15 +0000

    base geometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Defines an empty class
+"""
+
+
+class BaseGeometry:
+    """
+    Defines an empty class
+    """
+
+
+    pass
---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defines an empty class
+"""
+
+
+class BaseGeometry:
+    """
+    Defines an empty class
+    """
+    def area(self):
+        raise Exception('area() is not implemented')

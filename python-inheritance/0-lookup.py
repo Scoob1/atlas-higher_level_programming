8fb1fcb0fb436db4cdade12cc5c19980ce1600fd
Scoob1 <tamara_walling@yahoo.com>
2024-05-21 20:54:38 +0000

    dirobj

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines a function that returns a lis
+"""
+
+def lookup(obj):
+    """
+    This function returns a list of attributes and methods
+    """
+    return dir(obj)

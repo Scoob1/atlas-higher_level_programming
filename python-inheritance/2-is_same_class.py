660636aa52e77dffb6d452b2a133c4dc47164076
Scoob1 <tamara_walling@yahoo.com>
2024-05-21 21:06:36 +0000

    identical

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines a function that checks if classes are identical
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    This function checks if a class is identical to another
+    """
+    return (type(obj) is a_class)

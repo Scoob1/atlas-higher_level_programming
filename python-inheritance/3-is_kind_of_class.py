e88ca7551b78df44a43f0ffaa78afb44ced90352
Scoob1 <tamara_walling@yahoo.com>
2024-05-21 21:15:53 +0000

    lines too long

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-This module defines a function that checks if a class is inherited from a specified class
+This module defines a function that checks
+if a class is inherited from a specified class
 """
 
 
 def is_kind_of_class(obj, a_class):
     """
-    Returns true if the object is an instance of a class that inherited from, th specified class; otherwise false.
+    Returns true if the object is an instance of a class that inherited from,
+    the specified class; otherwise false.
     """
     return isinstance(obj, a_class)

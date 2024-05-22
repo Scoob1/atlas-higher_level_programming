a6bf39d06cb54ba79fd4d80485ae780a8810ee44
Scoob1 <tamara_walling@yahoo.com>
2024-05-22 03:10:20 +0000

    JSON string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+module that provides a function to return an object represented by JSON string
+"""
+import json
+
+def from_json_string(my_str):
+    """
+    Returns an object repesented by JSON string
+
+    Args:
+    my_str (str): The JSON string.
+
+    Returns:
+    The Python structure represented by thte JSON string.
+    """
+
+    return json.loads(my_str)

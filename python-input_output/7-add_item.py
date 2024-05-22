233bff41b45adb80a79b30e9faab65370873f9f1
Scoob1 <tamara_walling@yahoo.com>
2024-05-22 03:36:38 +0000

    add item delete pass

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,16 +3,18 @@
 module that adds all arguments to a Python list, and then saves them to a file
 """
 import sys
+import json
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 fname = "add_item.json"
-MyList = []
 
 try:
     MyList = load_from_json_file(fname)
-except:
-    pass
+except (FileNotFoundError, json.JSONDecodeError):
+    MyList = []
 
 for i in range(1, len(sys.argv)):
     MyList.append(sys.argv[i])

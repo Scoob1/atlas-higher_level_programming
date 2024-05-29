a100f2ecff8267cbba83c6b01e32d9a4e0f1c9f0
Scoob1 <tamara_walling@yahoo.com>
2024-05-29 23:59:39 +0000

    literally copy and pasted my last code

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -8,13 +8,13 @@ def pascal_triangle(n):
     """This function populates lists for pascal's triangle"""
     if n <= 0:
         my_list = []
-        return my_list[]
-    
+        return my_list
+
     new_list = []
     for i in range(n):
         row = [1] * (i + 1)
         if i >= 2:
-            for j in range[1, i]:
+            for j in range(1, i):
                 row[j] = new_list[i - 1][j - 1] + new_list[i - 1][j]
         new_list.append(row)
     return new_list

d839902548bfe617ec20f7327d0de2404861b293
Scoob1 <tamara_walling@yahoo.com>
2024-05-29 23:57:51 +0000

    fixed line deleted spacing

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -7,10 +7,9 @@ This module defines a function that populates lists for pascal's triangle
 def pascal_triangle(n):
     """This function populates lists for pascal's triangle"""
     if n <= 0:
-        rst - []
+        my_list = []
         return my_list[]
-
-
+    
     new_list = []
     for i in range(n):
         row = [1] * (i + 1)

c0e9348e45da6c9356ccfdae72bbfbc293cc1e04
Scoob1 <tamara_walling@yahoo.com>
2024-06-19 16:18:24 +0000

    lists states with N

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""Create states table in hbtn_0e_0_usa with some data"""
-
+""" Write a script that lists all states from the database hbtn_0e_0_usa """
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    result_states = cursor.fetchall()
+
+    for state in result_states:
+        print(state)
+
+    cursor.close()
+    db.close()

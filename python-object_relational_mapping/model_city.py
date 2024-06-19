b9e6c255bbf7e9373bf6b91d5a24cc9a2dff56ce
Scoob1 <tamara_walling@yahoo.com>
2024-06-19 23:53:53 +0000

    models city like state

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Python file similar to model_state.py named model_city.py that
+contains the class definition of a City."""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
+
+
+class City(Base):
+    """defines each city
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"). nullable=False)

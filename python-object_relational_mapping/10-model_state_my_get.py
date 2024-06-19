fdddbac9ff85b7a9b75cfe352a951758ea385be3
Scoob1 <tamara_walling@yahoo.com>
2024-06-19 17:52:15 +0000

    state object w name

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" List all State objects whos name match passed in argument in a database """
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    instance = session.query(State).filter(State.name == (sys.argv[4],))
+
+    if instance:
+        print(instance[0].id)
+    else:
+        print("Not found")

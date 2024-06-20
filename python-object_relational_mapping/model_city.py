30e96bde7330189961e334e910cad1dd359b4979
Scoob1 <tamara_walling@yahoo.com>
2024-06-20 00:10:01 +0000

    added foreign key modelscity

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ Python file similar to model_state.py named model_city.py that
 contains the class definition of a City."""
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
+
+Base = declarative_base()
 
 
 class City(Base):
-    """defines each city
-    """
+    """defines each city"""
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    id = Column(Integer, primary_key=True, unique=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)

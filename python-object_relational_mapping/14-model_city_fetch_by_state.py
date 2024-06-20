5d575b7b3d89182535e95cac1652fd275774fa66
Scoob1 <tamara_walling@yahoo.com>
2024-06-20 00:14:02 +0000

    city id order

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ prints all City objects from the database hbtn_0e_14_usa"""
-from model_state import Base, State
+from model_state import State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_city import city
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     for inst in (session.query(State.name, City.id, City.name)
-                 .filter(State.id == City.state_id)):
+            .filter(State.id == City.state_id).order_by(City.id)):
         print(inst[0] + ": (" + str(inst[1]) + ") " + inst[2])

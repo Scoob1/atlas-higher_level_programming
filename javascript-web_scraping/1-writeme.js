e3052bbedb62fa609f37a3f70d6e47d206ed06e5
nooboftime <tamara_walling@yahoo.com>
2024-08-01 15:18:22 -0500

    script that writes a string to a file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,1 +1,10 @@
-uhjh
+#!/usr/bin/node
+const fs = require('fs');
+const fp = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(fp, content, 'utf8', (error) => {
+  if (error) {
+    console.error(error);
+  }
+});

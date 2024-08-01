53c542e9fdfb57de56a10c927e95b0444c578721
nooboftime <tamara_walling@yahoo.com>
2024-08-01 15:36:04 -0500

    passing string

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const fp = process.argv[2];
 const string = process.argv[3];
 
-fs.writeFile(fp, content, 'utf8', (error) => {
+fs.writeFile(fp, string, 'utf8', (error) => {
   if (error) {
     console.error(error);
   }

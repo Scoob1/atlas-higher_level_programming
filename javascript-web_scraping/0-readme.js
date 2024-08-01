9bda7ed2b3e740861f92672bb941344161dede5a
nooboftime <tamara_walling@yahoo.com>
2024-08-01 15:05:02 -0500

    fixed semistandard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const fp = process.argv[2];
 
 fs.readFile(fp, 'utf8', (error, data) => {
-    if (error) {
-        console.log(error);
-        return;
-    }
-    console.log(data);
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(data);
 });

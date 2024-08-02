5fc69539e468202d8557580d80fcf3efe0aeacbe
nooboftime <tamara_walling@yahoo.com>
2024-08-01 19:45:56 -0500

    trying to fix the url

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-const film = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const url = process.argv[2];
 const characterId = '18';
 
 request(url, function (error, response, body) {
